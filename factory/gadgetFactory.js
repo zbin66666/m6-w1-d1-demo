ce555537bb6fffb865c7717f1f247ed7700f91e9
zbin66666 <binz6@uci.edu>
2023-09-25 19:00:32 -0700

    finished

---
+++ factory/gadgetFactory.js
@@ -0,0 +1,12 @@
+const Laptop = require("./laptop");
+const Tablet = require("./tablet");
+
+const gadget = { Laptop, Tablet };
+
+// add module.export here
+module.exports = {
+    createGadget(type, attributes) {
+    const GadgetType = gadget[type];
+    return new GadgetType(attributes);
+    }
+}
